4bc5adc14e510ba85563b646f06a02c46b464ae3
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-11-13 17:27:36 +0700

    cr lab1-1 and lab2

--- template-master/lab1-2/src/main.c
+++ template-master/lab1-2/src/main.c
@@ -1,5 +1,103 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+enum {max_p_len = 16, w_len = max_p_len * 2};
+
+typedef struct SEARCH_PATTERN {
+    char str[max_p_len + 2];
+    int len;
+    char bad;
+    unsigned shift_table[max_p_len];
+} search_pattern;
+
+typedef struct SEARCH_FIELD {
+    char str[w_len];
+    size_t st_i, end_i;
+    FILE* in;
+} search_field;
+
+void fill_shift_table(search_pattern* p) {
+    int i = 1; int j = 0;
+    while (p->len > i) {
+        if (p->str[j] == p->str[i]) {
+            p->shift_table[i] = ++j; ++i;
+        }
+        else {
+            if (j == 0) {
+                p->shift_table[i] = 0; ++i;
+            }
+            else
+                j = p->shift_table[j - 1];
+        }
+    } 
+}
+
+search_pattern create_s_pattern(FILE* in) {
+    search_pattern tmp = {.str = {0}};
+    if (fgets(tmp.str, max_p_len + 2, in) == 0)  tmp.bad = 1; // +2 чтобы даже при макс длинне паттерна прочитать \n
+    tmp.len = strlen(tmp.str) - 1;
+    if (tmp.len == 0) tmp.bad = 1;
+    tmp.str[tmp.len] = '\0';
+    fill_shift_table(&tmp);
+    return tmp;
+}
+
+search_field create_s_window(FILE* in) {
+    search_field tmp = {.str = {0}};
+    for (int i = 0; i < w_len; ++i) tmp.str[i] = fgetc(in);
+    tmp.st_i = 0; tmp.end_i = w_len - 1;
+    tmp.in = in;
+    return tmp;
+}
+
+char getc_window(search_field* w, size_t index) {
+    if (index > w->end_i) {
+        memmove(w->str, &w->str[max_p_len], max_p_len);
+        for (int i = 0; i < max_p_len; ++i)
+            w->str[max_p_len + i] = fgetc(w->in);
+        w->st_i += max_p_len; w->end_i += max_p_len;
+    }
+    return w->str[index - w->st_i];
+}
+
+void find_substring(FILE* in) {
+    search_pattern p = create_s_pattern(in);
+    search_field w = create_s_window(in);
+
+    for (int i = 0; i < p.len; ++i)
+        printf("%u ", p.shift_table[i]);
+    
+    if (p.bad) return;
+
+    size_t i = 0;
+    unsigned j = 0;
+    for(;;) {
+        size_t x = i + p.len - j - 1;
+        if (getc_window(&w, x) == EOF) 
+            return;
+
+        if (p.str[j] == getc_window(&w, i)) {
+            ++i; ++j;
+        }
+        else {
+            if (j > 0) {
+                printf("%zu %u ", i + 1 - j, j);
+                j = p.shift_table[j - 1];
+            }
+            else
+                ++i;
+        }
+    }
+
+}
+
+int main()
+{
+    FILE* in = fopen("in.txt", "r");
+    if (in == NULL) exit(0);
+
+    find_substring(in);
+
+    exit(0);
 }
--- template-master/lab2/src/main.c
+++ template-master/lab2/src/main.c
@@ -1,5 +1,64 @@
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+enum { max_input = 10 };
+
+void swap(char* a, char* b) { char tmp = *a; *a = *b; *b = tmp; }
+
+int is_good(char* str, int len) {
+    for (int i = 0; i < len; ++i) {
+        if (!( ('0' <= str[i]) && (str[i] <= '9') )) { 
+            return 0;
+        }
+        for (int j = i +1; j < len; ++j)
+            if (str[i] == str[j])
+                return 0;
+    }
+    return 1;
+}
+
+int perm(char* str, int len) {
+    int i = -1, j = -1;
+    for(int k = len - 2; k >= 0; --k) {
+        if(str[k] < str[k + 1]) {
+            i = k;
+            break;
+        }
+    }
+    if (i == -1) return 0;
+    for(int k = len - 1; k >= 0; --k) {
+        if(str[k] > str[i]) {
+            j = k;
+            break;
+        }
+    }
+    if (j == -1) return 0;
+    swap(&str[i], &str[j]);
+    for (int k = i + 1, x = 1; x <= (len - i - 1) / 2; ++k, ++x) {
+        swap(&str[k], &str[len - x]);
+    }
+    return 1;
+}
+
+int main()
+{
+    FILE* in = fopen("in.txt", "r");
+    FILE* out = fopen("out.txt", "w");
+    char str[max_input + 1] = {0};
+    int x;
+    char c;
+    for(int i = 0;(c = fgetc(in)) != '\n'; ++i) {
+        if (i == 10) { fprintf(out, "bad input"); exit(0);}
+        str[i] = c;
+    }
+    int len = strlen(str);
+    if (!is_good(str, len) || !fscanf(in, "%d", &x) ) { fprintf(out, "bad input"); exit(0);}
+
+    for(int i = 0; i < x; ++i) {
+        if(!perm(str, len)) break;
+        fprintf(out, "%s\n", str);
+    }
+
+    exit(0);
 }
