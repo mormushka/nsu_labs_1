8046834efa2502d1d2441ab762b6aab85876e282
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-11-18 23:40:01 +0700

    create stack and function for it(push, pop, show)

--- template-master/lab4/src/main.c
+++ template-master/lab4/src/main.c
@@ -1,5 +1,40 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+typedef struct STACK {
+    int data;
+    struct STACK* next;
+} stack;
+
+void push(stack** top, int data) {
+    stack* tmp = malloc(sizeof(stack));
+    tmp->data = data;
+    tmp->next = *top;
+    *top = tmp;
+}
+
+void pop(stack** top) {
+    if (*top == NULL)
+        return;
+    stack* tmp = (*top)->next;
+    free(top);
+    *top = tmp;
+}
+
+void show(stack* top) {
+    stack* tmp = top;
+    while (tmp != NULL) {
+        printf("%d ", tmp->data);
+        tmp = tmp->next;
+    }
+}
+
+int main() {
+    stack* s = NULL;
+    push(&s, 1);
+    push(&s, 2);
+    show(s);
+    printf("\n");
+    pop(&s); 
+    show(s);
 }
