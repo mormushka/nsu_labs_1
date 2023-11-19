b04601cce8d2a68a84ed1b43e22290507c3e955f
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-11-19 15:11:40 +0700

    added syntax checking function

--- template-master/lab4/src/main.c
+++ template-master/lab4/src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define LEN_OPPR 8
+
+const struct {const char operator; const int priority;} op_prior[LEN_OPPR] = {
+    ['+' % LEN_OPPR] = {'+', 1},
+    ['-' % LEN_OPPR] = {'-', 1},
+    ['*' % LEN_OPPR] = {'*', 2},
+    ['/' % LEN_OPPR] = {'/', 2},
+    ['(' % LEN_OPPR] = {'(', 0},
+    [')' % LEN_OPPR] = {')', 0}
+};
+
 typedef struct STACK {
     int data;
     struct STACK* next;
@@ -29,9 +40,35 @@ void show(stack* top) {
     }
 }
 
+char suntax_is_normal(FILE* in) {
+    char c;
+    c = fgetc(in);
+    while(!feof(in)) {
+        if (!( (c >= '0') && (c <= '9') ))
+            if (op_prior[c % LEN_OPPR].operator != c)
+                return 0;
+        c = fgetc(in);
+    }
+    return 1;
+}
+
+int calc(FILE* in, FILE* out) {
+    stack* numbers = NULL;
+    stack* operators = NULL; 
+
+    if (!suntax_is_normal(in)) { 
+        fprintf(out, "syntax error");
+        exit(0);
+    }
+
+    
+    
+}
+
 int main() {
     FILE* in = fopen("in.txt", "r");
     FILE* out = fopen("out.txt", "w");
     if ((in == NULL) || (out == NULL)) exit(0);
-    
+    calc(in, out);
+
 }
