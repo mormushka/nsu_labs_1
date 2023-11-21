d989abbbb6d3ed0bb62dd02869952cd32137d44a
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-11-21 16:59:21 +0700

    add support brackets and expection handlings (code passed all tests)

--- template-master/lab4/src/main.c
+++ template-master/lab4/src/main.c
@@ -1,24 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define STOP_WORK(TEXT) { printf(TEXT); exit(0); }
 #define LEN_OPPR 8
 #define END_IN(CHAR, IN) (((CHAR) == '\n') || feof(IN))
 #define HESH(OBJ) (OBJ) % LEN_OPPR
 #define PRIOR(OBJ) op_inf[(OBJ) % LEN_OPPR].priority
-#define CALC_ON_STACK(NUMS, OPRS) push(&NUMS, op_inf[HESH(pick_up(&OPRS))].func(pick_up(&NUMS), pick_up(&NUMS)));
+#define CALC_ON_STACK(NUMS, OPRS) { \
+    if ((NUMS == NULL) || (NUMS->next == NULL)) STOP_WORK("syntax error");\
+    if (OPRS == NULL) STOP_WORK("syntax error");\
+    push(&NUMS, op_inf[HESH(pick_up(&OPRS))].func(pick_up(&NUMS), pick_up(&NUMS))); \
+}
 
 int f_plus     (int a, int b) { return a + b; }
 int f_minus    (int a, int b) { return a - b; }
 int f_myltiply (int a, int b) { return a * b; }
-int f_division (int a, int b) { return a / b; }
+int f_division (int a, int b) { if (!b) STOP_WORK("division by zero"); 
+                                return a / b; }
 
 const struct {const char operator; const int priority; int (*const func) (int, int);} op_inf[LEN_OPPR] = {
     [HESH('+')] = {'+', 1, f_plus},
     [HESH('-')] = {'-', 1, f_minus},
     [HESH('*')] = {'*', 2, f_myltiply},
     [HESH('/')] = {'/', 2, f_division},
-    [HESH('(')] = {'(', 0},
-    [HESH(')')] = {')', -1}
+    [HESH('(')] = {'(', 0, NULL},
+    [HESH(')')] = {')', 0, NULL}
 };
 
 typedef struct STACK {
@@ -58,14 +64,16 @@ void show(stack* top, char* x) {
 char suntax_is_normal(FILE* in) {
     char c;
     c = fgetc(in);
+    char f = 0;
     while(!END_IN(c, in)) {
+        f = 1;
         if (!( (c >= '0') && (c <= '9') ))
             if (op_inf[c % LEN_OPPR].operator != c)
                 return 0;
         c = fgetc(in);
     }
     fseek(in, 0, SEEK_SET);
-    return 1;
+    return 1 * f;
 }
 
 void get_next_object(FILE* in, int* obj, char* it_num) {
@@ -88,45 +96,65 @@ void get_next_object(FILE* in, int* obj, char* it_num) {
     }
 }
 
-int calc(FILE* in, FILE* out) {
+int calc(FILE* in) {
     stack* numbers = NULL;
     stack* operators = NULL; 
 
-    if (!suntax_is_normal(in)) { 
-        fprintf(out, "syntax error");
-        exit(0);
-    }
+    if (!suntax_is_normal(in)) 
+        STOP_WORK("syntax error");
 
     char it_num;
     int obj;
     get_next_object(in, &obj, &it_num);
-    // no brackets yet
+    
     for (;;) {
-        if (it_num)
+        if (it_num) {
             push(&numbers, obj);
+            get_next_object(in, &obj, &it_num);
+        }
         else {
             if (END_IN(obj, in)) {
                 while (operators != NULL)
                     CALC_ON_STACK(numbers, operators);
                 break;
             }
+            else if (obj == '(') {
+                push(&operators, obj);
+                get_next_object(in, &obj, &it_num);
+                if (obj == ')') STOP_WORK("syntax error");
+            }
+            else if (obj == ')') {
+                if ((operators == NULL)) STOP_WORK("syntax error");
+                if (operators->data == '(') {
+                    pop(&operators);
+                }
+                else {
+                    while (operators->data != '(') {
+                        CALC_ON_STACK(numbers, operators);
+                        if (operators == NULL) STOP_WORK("syntax error");
+                    }
+                    pop(&operators);
+                }
+                get_next_object(in, &obj, &it_num);
+            }
             else {
                 while ((operators != NULL) && (PRIOR(obj) <= PRIOR(operators->data)))
                     CALC_ON_STACK(numbers, operators);
                 push(&operators, obj);
+                get_next_object(in, &obj, &it_num);
             }
         }
-        get_next_object(in, &obj, &it_num);
     }
 
-    show(numbers, "%d ");
-    show(operators, "%c ");
+    return numbers->data;
+    //show(numbers, "%d ");
+    //show(operators, "%c ");
 }
 
 int main() {
     FILE* in = fopen("in.txt", "r");
-    FILE* out = fopen("out.txt", "w");
-    if ((in == NULL) || (out == NULL)) exit(0);
-    calc(in, out);
+    if ((in == NULL)) exit(0);
+
+    printf("%d", calc(in));
 
 }
