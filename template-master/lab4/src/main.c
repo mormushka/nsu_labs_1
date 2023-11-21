86e7b3ced9ab3f54762aedaf4e3d04f4690164c2
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-11-21 20:37:21 +0700

    code refactoring (crazy macros)

--- template-master/lab4/src/main.c
+++ template-master/lab4/src/main.c
@@ -1,15 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define O2X {0,f_NAO},{0,f_NAO},
+#define O8X O2X O2X O2X O2X
+#define O32X O8X O8X O8X O8X
+#define O128X O32X O32X O32X O32X
+#define O256X O128X O128X
+
 #define STOP_WORK(TEXT) { printf(TEXT); exit(0); }
-#define LEN_OPPR 8
+#define LEN_OPPR 256
 #define END_IN(CHAR, IN) (((CHAR) == '\n') || feof(IN))
-#define HESH(OBJ) (OBJ) % LEN_OPPR
 #define PRIOR(OBJ) op_inf[(OBJ) % LEN_OPPR].priority
 #define CALC_ON_STACK(NUMS, OPRS) { \
     if ((NUMS == NULL) || (NUMS->next == NULL)) STOP_WORK("syntax error");\
     if (OPRS == NULL) STOP_WORK("syntax error");\
-    push(&NUMS, op_inf[HESH(pick_up(&OPRS))].func(pick_up(&NUMS), pick_up(&NUMS))); \
+    push(&NUMS, op_inf[pick_up(&OPRS)].func(pick_up(&NUMS), pick_up(&NUMS))); \
 }
 
 int f_plus     (int a, int b) { return a + b; }
@@ -17,14 +22,16 @@ int f_minus    (int a, int b) { return a - b; }
 int f_myltiply (int a, int b) { return a * b; }
 int f_division (int a, int b) { if (!b) STOP_WORK("division by zero"); 
                                 return a / b; }
+int f_NAO      (int a, int b) { STOP_WORK("syntax error"); }
 
-const struct {const char operator; const int priority; int (*const func) (int, int);} op_inf[LEN_OPPR] = {
-    [HESH('+')] = {'+', 1, f_plus},
-    [HESH('-')] = {'-', 1, f_minus},
-    [HESH('*')] = {'*', 2, f_myltiply},
-    [HESH('/')] = {'/', 2, f_division},
-    [HESH('(')] = {'(', 0, NULL},
-    [HESH(')')] = {')', 0, NULL}
+const struct name{const int priority; int (*const func) (int, int);} op_inf[LEN_OPPR] = {
+    O256X
+    ['+'] = {1, f_plus},
+    ['-'] = {1, f_minus},
+    ['*'] = {2, f_myltiply},
+    ['/'] = {2, f_division},
+    ['('] = {0, NULL},
+    [')'] = {0, NULL}
 };
 
 typedef struct STACK {
@@ -53,27 +60,11 @@ int pick_up(stack** top) {
     return tmp;
 }
 
-char suntax_is_normal(FILE* in) {
-    char c;
-    c = fgetc(in);
-    char f = 0;
-    while(!END_IN(c, in)) {
-        f = 1;
-        if (!( (c >= '0') && (c <= '9') ))
-            if (op_inf[c % LEN_OPPR].operator != c)
-                return 0;
-        c = fgetc(in);
-    }
-    fseek(in, 0, SEEK_SET);
-    return f;
-}
-
 void get_next_object(FILE* in, int* obj, char* it_num) {
-    char c = fgetc(in);
-    if ((c >= '0') && (c <= '9')) {
-        *it_num = 1;
+    char c = *obj = fgetc(in);
+    *it_num = ((c) >= '0') && (c <= '9');
+    if (*it_num) {
         *obj = 0;
-
         do {
             *obj *= 10;
             *obj += c - '0';
@@ -82,17 +73,11 @@ void get_next_object(FILE* in, int* obj, char* it_num) {
         if(!END_IN(c, in))
             fseek(in, ftell(in) - 1, SEEK_SET);
     }
-    else {
-        *it_num = 0;
-        *obj = c;
-    }
 }
 
 int calc(FILE* in) {
     stack* numbers = NULL;
-    stack* operators = NULL; 
-
-    if (!suntax_is_normal(in)) STOP_WORK("syntax error");
+    stack* operators = NULL;
 
     char it_num;
     int obj;
@@ -128,6 +113,8 @@ int calc(FILE* in) {
         }
         get_next_object(in, &obj, &it_num);
     }
+    if (numbers == NULL)
+        STOP_WORK("syntax error");
     return numbers->data;
 }
 
