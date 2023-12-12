9f6593a422d67eeaf79a38b5f70c2db662b2f357
mormushka <roman_tikhonov_0_0.@mail.ru>
2023-12-13 01:06:41 +0700

    refactoring

--- template-master/lab2/src/main.c
+++ template-master/lab2/src/main.c
@@ -2,63 +2,75 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum { max_input = 10 };
+#define MAX_INPUT 10
 
 void swap(char* a, char* b) { char tmp = *a; *a = *b; *b = tmp; }
 
 int is_good(char* str, int len) {
+    char num_nums[10] = {0};
     for (int i = 0; i < len; ++i) {
-        if (!( ('0' <= str[i]) && (str[i] <= '9') )) { 
+        if (!( ('0' <= str[i]) && (str[i] <= '9') ))
             return 0;
-        }
-        for (int j = i +1; j < len; ++j)
-            if (str[i] == str[j])
-                return 0;
+        if (num_nums[str[i] - '0']++)
+            return 0;
     }
     return 1;
 }
 
+void reverse (char* str, int start_i, int end_i) {
+    for (; start_i < end_i; start_i++, end_i--) 
+        swap(&str[start_i], &str[end_i]);
+}
+
+int search_i (char* str, int len) {
+    for (int i = len - 2; i >= 0; --i)
+        if (str[i] < str[i + 1])
+            return i;
+    return -1;
+}
+
+int search_j (char* str, int len, int i) {
+    for(int j = len - 1; j >= 0; --j)
+        if(str[j] > str[i])
+            return j;
+    return -1;
+}
+
 int perm(char* str, int len) {
-    int i = -1, j = -1;
-    for(int k = len - 2; k >= 0; --k) {
-        if(str[k] < str[k + 1]) {
-            i = k;
-            break;
-        }
-    }
-    if (i == -1) return 0;
-    for(int k = len - 1; k >= 0; --k) {
-        if(str[k] > str[i]) {
-            j = k;
-            break;
-        }
-    }
-    if (j == -1) return 0;
+    int i, j;
+    if ((i = search_i(str, len)) == -1) return 0;
+    if ((j = search_j(str, len, i)) == -1) return 0;
     swap(&str[i], &str[j]);
-    for (int k = i + 1, x = 1; x <= (len - i - 1) / 2; ++k, ++x) {
-        swap(&str[k], &str[len - x]);
-    }
+    reverse (str, i + 1, len - 1);
     return 1;
+}
+
+int perms(char* str, int len, int num) {
+    for(int i = 0; i < num; ++i) {
+        if(!perm(str, len)) 
+            break;
+        printf("%s\n", str);
+    }
 }
 
 int main()
 {
     FILE* in = fopen("in.txt", "r");
-    FILE* out = fopen("out.txt", "w");
-    char str[max_input + 1] = {0};
-    int x;
-    char c;
-    for(int i = 0;(c = fgetc(in)) != '\n'; ++i) {
-        if (i == 10) { fprintf(out, "bad input"); exit(0);}
-        str[i] = c;
-    }
-    int len = strlen(str);
-    if (!is_good(str, len) || !fscanf(in, "%d", &x) ) { fprintf(out, "bad input"); exit(0);}
+    if (in == NULL) 
+        exit(0);
 
-    for(int i = 0; i < x; ++i) {
-        if(!perm(str, len)) break;
-        fprintf(out, "%s\n", str);
+    char str[MAX_INPUT + 2];
+    int n;
+
+    fscanf(in, "%11[^\n]", str);
+    int len = strlen(str);
+
+    if (!is_good(str, len) || !fscanf(in, "%d", &n) ) { 
+        printf("bad input");
+        exit(0);
     }
+
+    perms(str, len, n);
 
     exit(0);
 }
