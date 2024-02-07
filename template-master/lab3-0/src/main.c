fe3f47f25073eb605c359de2a49d10c47931832c
mormushka <roman_tikhonov_0_0.@mail.ru>
2024-02-07 14:40:06 +0700

    heap sort!

--- template-master/lab3-0/src/main.c
+++ template-master/lab3-0/src/main.c
@@ -1,5 +1,60 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-    return EXIT_SUCCESS;
+void swap(int* a, int* b) { 
+    int tmp = *a; 
+    *a = *b; 
+    *b = tmp; 
+}
+
+void heapify(int* a, const int n, const int i) {
+	int largest = i;
+
+	int left = 2*i + 1;
+	int right = 2*i + 2;
+
+	if ((left < n) && (a[left] > a[largest]))
+		largest = left;
+	if ((right < n) && (a[right] > a[largest]))
+		largest = right;
+
+	if (largest != i) {
+		swap(&a[i], &a[largest]);
+		heapify(a, n, largest);
+	}
+}
+
+void heap_sort(int* a, const int n) {
+	// build max heap
+	for (int i = n / 2 - 1; i >= 0; i--)
+		heapify(a, n, i);
+
+	// heap sort
+	for (int i = n - 1; i >= 0; i--) {
+		swap(&a[0], &a[i]);
+		heapify(a, i, 0);
+	}
+}
+
+int main()
+{
+    size_t n;
+    if (!scanf("%zu", &n))
+        return 0;
+
+    int* a = (int *)malloc(n * sizeof(int));
+    for (size_t i = 0; i < n; ++i) {
+        if(!scanf("%d", &a[i])) {    
+            free(a);
+            return 0;
+        }
+    }
+    
+    heap_sort(a, n);
+
+    for(size_t i = 0; i < n; ++i) 
+        printf("%d ", a[i]);
+    
+    free(a);
+    return 0;
 }
